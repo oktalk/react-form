009fefe7fb682645b8702c1ff10e7ad6ed82572c
Justin Stevens <justin@interactiveshift.com>
2017-08-30 17:10:11 -0500

    Parsing files into layouts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HelloWorld from './HelloWorld';
 import './App.css';
 
 const App = () => {
-  //foobar
-  return ( <div className="App"> Hello World! </div>);
+  return (
+    <div className="App">
+      <HelloWorld name="Foo" />
+      <HelloWorld name="Bar" />
+      </div>
+  );
 };
+
+export default App;
