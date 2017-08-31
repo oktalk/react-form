6f86ec09a19b2a15339d73b215a18605258f2d49
Justin Stevens <justin@interactiveshift.com>
2017-08-31 12:51:29 -0500

    remove group

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -5,19 +5,25 @@ class FormGroup extends Component {
   constructor(props) {
     super(props);
     this.state = {greeting: 'Hello'};
-    // this.frenchify = this.frenchify.bind(this);
+    this.frenchify = this.frenchify.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
   }
 
   frenchify() {
     this.setState({greeting: 'Bonjour'});
   }
 
+  removeGreeting() {
+    this.props.removeGreeting(this.props.name);
+  }
+
   render() {
     return (
       <div className="form-group">
-      {this.state.greeting} {this.props.name}!
-      <br />
-      <button onClick={this.frenchify.bind(this)}>Frenchify!</button>
+        {this.state.greeting} {this.props.name}!
+        <br />
+        <button onClick={this.frenchify}>Frenchify!</button>
+        <button onClick={this.removeGreeting}>Remove</button>
       </div>
     );
   }
