872552fd7852ed8448281cb3a30141d1bed98376
Justin Stevens <justin@interactiveshift.com>
2017-08-31 12:51:09 -0500

    Render all additional groups

--- src/components/form-list.js
+++ src/components/form-list.js
@@ -1,22 +1,41 @@
 import React, {Component} from 'react';
 import FormGroup from './form-group';
+import AddGreeter from './greeter';
 import '../styles/form-list.css';
 
 class FormList extends Component {
   constructor(props) {
     super(props);
     this.state = {greetings: ['Jim', 'Sally', 'Buck']};
+    this.addGreeting = this.addGreeting.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
+  }
+
+  addGreeting(newName) {
+    this.setState({greetings: [...this.state.greetings, newName]});
+  }
+
+  removeGreeting(removeName) {
+    const filteredGreetings = this.state.greetings.filter(name => {
+      return name !== removeName;
+    });
+    this.setState({greetings: filteredGreetings});
   }
 
   renderGreetings() {
     return this.state.greetings.map(name => (
-      <FormGroup key={name} name={name} />
+      <FormGroup
+        key={name}
+        name={name}
+        removeGreeting={this.removeGreeting}
+      />
     ));
   }
 
   render() {
     return (
       <div className="form-list">
+        <AddGreeter addGreeting={this.addGreeting} />
         {this.renderGreetings()}
       </div>
     );
